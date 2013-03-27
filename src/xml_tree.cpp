c957350254cfa25e5529acba007ec3931c188790
Eric Haengel <eric.c.haengel@gmail.com>
2013-03-27 11:39:13 -0400

    Outlined the tree class

--- src/xml_tree.cpp
+++ src/xml_tree.cpp
@@ -1,1 +1,22 @@
 #include "xml_tree.h"
+
+XML_TreeNode::XML_TreeNode() {
+	//The data for if this is a tag node
+	start_tag = NULL;
+	end_tag = NULL;
+
+	//The content of this node
+	node_content.clear();
+
+	//The tree structure
+	parent = NULL;
+	children.clear();
+
+	next_sibling = NULL;
+	prev_sibling = NULL;
+}
+
+XML_TreeNode::~XML_TreeNode() {
+	//Do nothing
+}
+
--- src/xml_tree.h
+++ src/xml_tree.h
@@ -16,12 +16,51 @@ public:
 	XML_TreeNode();
 	~XML_TreeNode();
 
+	//////////////////////////////////
+	// Content management functions //
+	//////////////////////////////////
 
-	//The types of tree node
-	enum {
-		TAG_NODE,
-		CONTENT_NODE
-	};
+	//Modify the content
+	int SetNodeContent(vector<XML_Content> node_content);
+	int AddNodeContent(XML_Content new_content);
+
+	unsigned int GetNodeContentCount();
+	XML_Content GetNodeContent(unsigned int index);
+
+	//Recursively parse the content to create a subtree
+	int ParseContent();
+
+	///////////////////////////////////
+	// Tree search related functions //
+	///////////////////////////////////
+
+	//////////////////////////////////////
+	// Tree structure related functions //
+	//////////////////////////////////////
+
+	//Modify the start/end tag
+	int SetStartTag(XML_Tag* start_tag);
+	XML_Tag* GetStartTag();
+
+	int SetEndTag(XML_Tag* end_tag);
+	XML_Tag* GetEndTag();
+
+	//Modify the parent/child tags
+	int SetParentTag(XML_Tag* parent_tag);
+	XML_Tag* GetParentTag();
+
+	int SetChildrenTags(vector<XML_Tag*> children_tags);
+	int AddChildTag(XML_Tag* child_tag);
+
+	unsigned int GetChildrenTagCount();
+	XML_Tag* GetChildTag(unsigned int index);
+
+	//Modify the siblings
+	int SetNextSibling(XML_Tag* next_sibling_tag);
+	XML_Tag* GetNextSibling();
+
+	int SetPrevSibling(XML_Tag* prev_sibling_tag);
+	XML_Tag* GetPrevSibling();
 
 private:
 	//The data for if this is a tag node
